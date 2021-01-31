513b785016c0fe5c34f28a1a81aea5978235df0f
shane carvalho <shanec_@hotmail.com>
2021-01-31 13:40:17 +1100

    added crontab entry

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 echo "=> Installing fan control...\n"
 sudo cp fancontrol.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/fancontrol.sh
+
+(crontab -l ; echo "*/3 * * * * /usr/local/bin/fancontrol.sh")| crontab -
